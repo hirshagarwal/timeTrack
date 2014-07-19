3dbba86b4e3fb0e98a8d7ba7b1c9be94494a03d0
Hirsh A <hirsh.agarwal@gmail.com>
2014-07-18 23:21:46 -0400

    Updated: v.0.0.1 R6 	Created: createAccount.html 		Interface for creating an account 	Created: accounts.css 		Styling for createAccount.html 	Created: graph.php 		Testing for the homepage (and potentially future) graphs 	Added: servers.jpg 		Background image for createAccount.html 	Created: js/ 		All of the javascript used to run the site 	Modified: main.css 		Updated styling 	Modified: images 		Added images 	Modified: index.html 		Added graph 		Removed pricing 		Updated nav-bar 	Modified: createAccount.php 		Working script 		NOTE: Requires the login information for the database to work, this is private as it functions using our database, feel free to enter the credentials of your own for testing purposes.

--- php/createAccount.php
+++ php/createAccount.php
@@ -1,8 +1,17 @@
-<?
+<?php
+
+
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $username = $_POST['username'];
 $password = $_POST['password'];
+
+mysql_connect($host, $user, $pass) or die (mysql_error());
+mysql_select_db($db) or die (mysql_error());
+
+	mysql_query("INSERT INTO Users(id, username, password, email, organization, status, uid) VALUES ('NULL', '$username', 'password', '$email', 'NULL', '1', 'NULL')") or die (mysql_error());
+
+ header( 'Location:../');
 
 ?>
