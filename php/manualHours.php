097aab378ce454aa6c5bbf63bef2144f2d9dd10a
Hirsh A <hirsh.agarwal@gmail.com>
2014-07-19 15:39:03 -0400

    Added calculator for manual hour entry

--- php/manualHours.php
+++ php/manualHours.php
@@ -4,14 +4,33 @@ $start1 = $_POST['start1'];
 $end1 = $_POST['end1'];
 
 
-if($start1>$end1){
-$hours = $start1 - $end1;
-} else {
-	$hours = $end1 - $start1;
+function roundToQuarterHour($time){
+	$timeArray = explode(":", $time);
+	$minutes = $timeArray[1];
+	$rmin = $minutes%15;
+		if($rmin > 7){
+			$minutes = $minutes + (15-$rmin);
+		} else {
+			$minutes = $minutes - ($rmin);
+		}
+		if($minutes==60){
+			$minutes=00;
+			$timeArray[0]++;
+		}
+
+		$minutes = $minutes/60;
+	return $timeArray[0]+$minutes;
 }
 
+
+$start1 = roundToQuarterHour($start1);
+$end1 = roundToQuarterHour($end1);
+
+//echo $end1;
+
+$hours = $end1 - $start1;
+
+
 echo $hours;
-
-
 
 ?>
