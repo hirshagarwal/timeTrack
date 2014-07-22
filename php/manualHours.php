f27eb1e19323c13e1b28a38c231485905b57566f
jamiewitter <jamiewitter@gmail.com>
2014-07-21 21:34:17 -0400

    Update manualHours.php

--- php/manualHours.php
+++ php/manualHours.php
@@ -1,5 +1,12 @@
 <?php
-
+//Start session
+session_start();
+//Check whether the session variable 'username' is present.
+if(!isset($_SESSION['username']) || (trim($_SESSION['username'])) == ''))
+	{
+			header("location: login.html");
+			exit();
+	}
 $start1 = $_POST['start1'];
 $end1 = $_POST['end1'];
 $start2 = $_POST['start2'];
